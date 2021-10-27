decd41db435e7408b9ee13246ace5aa080dc5cc0
bclerc <bclerc@student.42.fr>
2021-10-27 09:23:56 +0000

    okdac

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: bclerc <bclerc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 16:28:32 by asgaulti          #+#    #+#             */
-/*   Updated: 2021/10/25 12:52:51 by bclerc           ###   ########.fr       */
+/*   Updated: 2021/10/25 16:01:59 by bclerc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ void	minishell(int ac, char **av, char **envp)
 		}
 		 add_history(str);
 		str = transform_str(str, envp);
-		parser(str, envp);
+		//parser(str, envp);
 		if (execute_commands(str, envp, 0) == -1)
 		{
 			free(prompt);
