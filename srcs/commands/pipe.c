ac62e7566540c8fb557e3d18e54b9352a0df6f06
Bryce CLERC <bclerc@student.42.fr>
2021-12-08 19:16:17 +0100

    Ca marche et ca marche pas, il est 19h.

--- srcs/commands/pipe.c
+++ srcs/commands/pipe.c
@@ -6,7 +6,7 @@
 /*   By: bclerc <bclerc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 12:36:56 by bclerc            #+#    #+#             */
-/*   Updated: 2021/12/08 17:08:27 by bclerc           ###   ########.fr       */
+/*   Updated: 2021/12/08 19:15:51 by bclerc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int get_pipe_count(t_cmd *cmd)
         i++;
         tmp = tmp->next;
     }
-    return (i);
+    return (i - 1);
 }
 
 int close_fd(int *tab_fd, int nb_pipes)
@@ -60,37 +60,36 @@ int m_pipe(t_cmd *cmd)
     pid_t pid;
     int status;
     t_cmd *tmp;
+    int nbpipe;
     int i;
-    int *tab_fd;
+    int tab_fd[500];
 
-    tab_fd = (int *)malloc(sizeof(int) * (get_pipe_count(cmd)*2));
+    nbpipe = get_pipe_count(cmd);
     tmp = cmd;
-    open_pipe(tab_fd, get_pipe_count(cmd));
+    open_pipe(tab_fd, nbpipe);
     i = 0;
     while (tmp)
     {
         pid = fork();
         if (pid == 0)
         {
-            printf("(%d) Executed cmd : %s\n", i-1, tmp->cmd);
+            printf("(%d) Executed cmd : '%s %s'\n", i-1, tmp->cmd, tmp->msg);
             if (tmp->next)
                 dup2(tab_fd[i + 1], 1);
-            if (i)
+            if (i != 0)
                 dup2(tab_fd[i - 2], 0);
-            close_fd(tab_fd, get_pipe_count(cmd));
+            close_fd(tab_fd, nbpipe);
             execve(ft_strjoin("/bin/", tmp->cmd), get_argv(tmp), env_to_char());
             perror("ERROR: ");
         }
         tmp = tmp->next;
         i=+2;
-
-    } 
-    close_fd(tab_fd, get_pipe_count(cmd));
+    }
+    close_fd(tab_fd, nbpipe);
     i = 0;
-    while (i < get_pipe_count(cmd) + 1)
+    while (i < nbpipe + 1)
     {
-        wait(&status);
-     
+        wait(NULL);
         i++;
     }
     exit(1);
