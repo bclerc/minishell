30a62698be009bdb41f048a4b25e4a32982ee582
Bryce CLERC <bclerc@student.42.fr>
2021-12-14 17:05:48 +0100

    Fix cmd mix btw built-in and execve

--- srcs/commands/pipe.c
+++ srcs/commands/pipe.c
@@ -6,7 +6,7 @@
 /*   By: bclerc <bclerc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 12:36:56 by bclerc            #+#    #+#             */
-/*   Updated: 2021/12/14 15:11:04 by bclerc           ###   ########.fr       */
+/*   Updated: 2021/12/14 17:04:56 by bclerc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,13 +37,14 @@ int m_pipe(t_cmd *cmd)
                 dup2(pipes[i - 2], 0); // copie l'entree standard i - 2 (i augmente de 2 si i = 2 i -2 = 0 entree standard
             close_fd(pipes, nbpipe); // On ferme les sortie du pipe, parce qu'on les utilises pas, ils sont la juste pour faire la lien entre le parent et le child
             execute_commands(tmp); // On execute la commande
+            exit(1);
         }
         tmp = tmp->next; // commande suivante
         i = i + 2;
     }
     close_fd(pipes, nbpipe); // Same line 81, on ferme les sorties du pipes.
     i = 0;
-    while (i < nbpipe + 1)
+    while (i < nbpipe + 2)
     {
         wait(&status);  // On attends tous les processus childs
         i++;
