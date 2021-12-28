3d60146282b3da922e05501b59c0309c41ffdf7c
Bryce CLERC <bclerc@student.42.fr>
2021-12-28 17:43:21 +0100

    norm

--- srcs/built-in/utils.c
+++ srcs/built-in/utils.c
@@ -6,15 +6,15 @@
 /*   By: bclerc <bclerc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 12:13:13 by bclerc            #+#    #+#             */
-/*   Updated: 2021/12/27 15:18:41 by bclerc           ###   ########.fr       */
+/*   Updated: 2021/12/28 17:42:57 by bclerc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-int is_eof(char *s)
+int	is_eof(char *s)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (s[i] != -1)
@@ -23,23 +23,19 @@ int is_eof(char *s)
 		return (1);
 	return (0);
 }
-int get_fd(char *path)
+
+int	get_fd(char *path)
 {
-	int fd;
-	struct stat buffer;
+	struct stat	buffer;
+	int			fd;
 
 	fd = 1;
 	if (path)
 	{
 		if (access(path, F_OK))
-		{
 			fd = open(path, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-		}
 		else
-		{
 			fd = open(path, O_RDONLY);
-			printf("FD successful open\n");
-		}
 	}
 	if (fd < 0)
 	{
